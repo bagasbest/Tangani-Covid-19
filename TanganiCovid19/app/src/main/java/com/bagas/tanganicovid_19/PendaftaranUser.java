03f1662413a2e4a8e483afdb10f844ef6d349193
bagasbest <bagasbest80@gmail.com>
2020-04-28 15:19:56 +0700

    Form Validation Regis User, Set Up Firebase

--- TanganiCovid19/app/src/main/java/com/bagas/tanganicovid_19/PendaftaranUser.java
+++ TanganiCovid19/app/src/main/java/com/bagas/tanganicovid_19/PendaftaranUser.java
@@ -1,19 +1,56 @@
 package com.bagas.tanganicovid_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PendaftaranUser extends AppCompatActivity {
+    EditText etNama, etEmail, etPassword;
+    RadioButton rbLaki, rbPerempuan;
+    String gender ="";
 
+    DatabaseReference databaseReference;
+    FirebaseAuth firebaseAuth;
+    AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pendaftaran_user);
+
+        //casting view
+        etNama = findViewById(R.id.et_regis_user);
+        etEmail = findViewById(R.id.et_regis_email);
+        etPassword = findViewById(R.id.et_regis_password);
+        rbLaki = findViewById(R.id.laki);
+        rbPerempuan = findViewById(R.id.perempuan);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void gotologin(View view) {
@@ -45,5 +82,58 @@ public class PendaftaranUser extends AppCompatActivity {
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+
+    public void btn_mendaftar(View view) {
+
+        awesomeValidation.addValidation(this, R.id.et_regis_user,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        awesomeValidation.addValidation(this, R.id.et_regis_email,
+                Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+
+        awesomeValidation.addValidation(this, R.id.et_regis_password,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_password);
+
+            if(awesomeValidation.validate()) {
+
+                final String username = etNama.getText().toString();
+                final String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (rbLaki.isChecked()) {
+                    gender = "Laki-laki";
+                }
+                if (rbPerempuan.isChecked()) {
+                    gender = "Perempuan";
+                }
+
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(PendaftaranUser.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Users information = new Users(
+                                            username, email, gender
+                                    );
+
+                                   FirebaseDatabase.getInstance().getReference("User")
+                                           .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                           .setValue(information).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                       @Override
+                                       public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(PendaftaranUser.this, "Registration Complete", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                       }
+                                   });
+
+                                } else {
+
+                                }
+                            }
+                        });
+            }
     }
 }
---
+++ TanganiCovid19/app/src/main/java/com/bagas/tanganicovid_19/Users.java
@@ -0,0 +1,15 @@
+package com.bagas.tanganicovid_19;
+
+public class Users {
+    public String username,Email, Gender;
+
+    public Users () {
+
+    }
+
+    public Users (String username, String email, String gender) {
+        this.username = username;
+        Email = email;
+        Gender = gender;
+    }
+}
